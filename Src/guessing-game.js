12dad9669905494dc6db23437b8b3fa309238a91
yana4erepko <119877422+yana4erepko@users.noreply.github.com>
2023-02-27 23:34:15 +0300

    Update and rename src/guessing-game.js to Src/guessing-game.js

---
+++ Src/guessing-game.js
@@ -0,0 +1,24 @@
+class { 
+    constructor() {
+    }
+
+    setRange(min, max)  {
+        this.min = min;
+        this.max = max;
+    }
+
+    guess() {
+        this.guessNum = this.min + Math.round((this.max - this.min) / 2);
+        return this.guessNum;
+    }
+
+    lower() {
+        this.max = this.guessNum;
+    }
+
+    greater() {
+        this.min = this.guessNum;
+    }
+}
+
+module.exports = GuessingGame;
